908e597c8cc2e229102d6abd45a12d8ba960708b
zhartaevem <zhartaevem@gmail.com>
2019-09-01 13:25:25 +0500

    Init

---
+++ BuyTicketToMars/App/App.vue
@@ -0,0 +1,42 @@
+﻿<template>
+    <div class="container">
+        <div class="header">
+            <header class="wrap header_wrap">
+                <div class="header_top">
+                    <div class="header_logo">
+                        <img src="images/solar_system.png" />
+                        <p class="evaa_font">FLIGHT</p>
+                    </div>
+                    <div class="header_buttons"></div>
+                </div>
+                <div class="header_middle">
+                    <h1>полет на марс</h1>
+                </div>
+                <div class="header_bottom">
+                    <div class="header_buy_button"></div>
+                </div>
+            </header>
+        </div>
+        <div class="header">
+            <header class="wrap header_wrap">
+                <div class="header_top">
+                    <div class="header_logo"></div>
+                    <div class="header_buttons"></div>
+                </div>
+                <div class="header_middle">
+                    <h1>ПОЛЕТ НА МАРС</h1>
+                </div>
+                <div class="header_bottom">
+                    <div class="header_buy_button"></div>
+                </div>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style lang="scss">
+</style>
